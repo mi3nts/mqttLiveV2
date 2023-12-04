dae6c83bf20467592d06cd38f56f91546d3f07c3
lakithaomal <lhw150030@utdallas.edu>
2023-12-04 18:13:51 +0000

    data reader updates

--- firmware/runDataReaders.sh
+++ firmware/runDataReaders.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-kill $(pgrep -f 'python3 DCDataReader.py')
+kill $(pgrep -f 'python3 liveDCReader.py')
 sleep 1
-python3 DCDataReader.py &
+python3 liveDCReader.py &
 
-kill $(pgrep -f 'python3 LNDataReader.py')
+kill $(pgrep -f 'python3 liveLNReader.py')
 sleep 1
-python3 LNDataReader.py &
+python3 liveLNReader.py &
 
 
